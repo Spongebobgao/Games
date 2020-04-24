740cc500757971175f7729c88531011550483ee4
Spongebobgao <spongebobgao@gmail.com>
2020-04-24 18:39:44 -0500

    methods work in different directions

--- src/components/Snake.vue
+++ src/components/Snake.vue
@@ -27,60 +27,116 @@
 export default {
   data() {
     return {
-      snake: [3622, 3623, 3624],
-      lastKeyPressed: "",
-      snakeHead: 0
+      snake: [3622, 3623, 3624, 3625, 3626],
+      turns: {},
+      interval: 0,
+      currentDirection: "",
+      priority: 0,
+      lastIndexOfSnake: 0
     };
   },
   methods: {
     startGame() {
+      this.resetData();
       window.addEventListener("keydown", this.controlSnake);
+    },
+    resetData() {
+      this.snake = [3622, 3623, 3624, 3625, 3626];
+      this.turns = {};
+      this.currentDirection = "";
+      this.priority = 0;
+      this.lastIndexOfSnake = this.snake.length - 1;
+      clearInterval(this.interval);
+      this.inter = 0;
       this.snake.forEach(snake =>
         document.getElementById(snake).classList.add("snake")
       );
+      document
+        .getElementById(this.snake[this.lastIndexOfSnake])
+        .classList.add("snakeHead");
     },
     controlSnake(e) {
+      document
+        .getElementById(this.snake[this.lastIndexOfSnake])
+        .classList.remove("snakeHead");
       this.snake.forEach(snake =>
         document.getElementById(snake).classList.remove("snake")
       );
       //go to the right
       if (e.code === "ArrowRight") {
-        this.snake = this.snake.map(value => {
-          return value + 1;
-        });
-        console.log(this.snake.toString());
-        this.snake.forEach(snake =>
-          document.getElementById(snake).classList.add("snake")
-        );
+        this.goRight("ArrowRight");
       }
       //go to the left
       if (e.code === "ArrowLeft") {
-        this.snake = this.snake.map(value => {
-          return value - 1;
-        });
-        this.snake.forEach(snake =>
-          document.getElementById(snake).classList.add("snake")
-        );
+        this.goLeft("ArrowLeft");
       }
       //go up
       if (e.code === "ArrowUp") {
-        this.snakeHead = this.snake[this.snake.length - 1];
-        this.snake[this.snake.length - 1] -= 100;
-        this.snake[0] += 1;
-        this.snake[1] += 1;
-        this.snake.forEach(snake =>
-          document.getElementById(snake).classList.add("snake")
-        );
+        this.goUp("ArrowUp");
       }
       //go down
       if (e.code === "ArrowDown") {
-        this.snakeHead = this.snake[this.snake.length - 1];
-        this.snake[this.snake.length - 1] += 100;
-        this.snake[0] += 1;
-        this.snake[1] += 1;
+        this.goDown("ArrowDown");
+      }
+    },
+    goRight(ArrowRight) {
+      if (this.currentDirection != "ArrowLeft") {
+        this.currentDirection = ArrowRight;
+        this.snake.shift();
+        this.lastIndexOfSnake--;
         this.snake.forEach(snake =>
           document.getElementById(snake).classList.add("snake")
         );
+        this.snake.push(this.snake[this.lastIndexOfSnake] + 1);
+        this.lastIndexOfSnake++;
+        document
+          .getElementById(this.snake[this.lastIndexOfSnake])
+          .classList.add("snakeHead");
+      }
+    },
+    goLeft(ArrowLeft) {
+      if (this.currentDirection != "ArrowRight") {
+        this.currentDirection = ArrowLeft;
+        this.snake.shift();
+        this.lastIndexOfSnake--;
+        this.snake.forEach(snake =>
+          document.getElementById(snake).classList.add("snake")
+        );
+        this.snake.push(this.snake[this.lastIndexOfSnake] - 1);
+        this.lastIndexOfSnake++;
+        document
+          .getElementById(this.snake[this.lastIndexOfSnake])
+          .classList.add("snakeHead");
+      }
+    },
+    goUp(ArrowUp) {
+      if (this.currentDirection != "ArrowDown") {
+        this.currentDirection = ArrowUp;
+        this.snake.shift();
+        this.lastIndexOfSnake--;
+        this.snake.forEach(snake =>
+          document.getElementById(snake).classList.add("snake")
+        );
+        this.snake.push(this.snake[this.lastIndexOfSnake] - 100);
+        this.lastIndexOfSnake++;
+        document
+          .getElementById(this.snake[this.lastIndexOfSnake])
+          .classList.add("snakeHead");
+      }
+    },
+    goDown(ArrowDown) {
+      if (this.currentDirection != "ArrowUp") {
+        this.currentDirection = ArrowDown;
+        this.snake.shift();
+        this.lastIndexOfSnake--;
+        this.snake.forEach(snake =>
+          document.getElementById(snake).classList.add("snake")
+        );
+        this.snake.push(this.snake[this.lastIndexOfSnake] + 100);
+        this.lastIndexOfSnake++;
+        document
+          .getElementById(this.snake[this.lastIndexOfSnake])
+          .classList.add("snakeHead");
       }
     }
   }
@@ -119,5 +175,12 @@ h3 {
 }
 .snake {
   background: #090;
+  border-radius: 25%;
+}
+.snakeHead {
+  background: url("../assets/snakeHead.jpg");
+  background-color: #090;
+  background-blend-mode: multiply;
+  border-radius: 50%;
 }
 </style>
