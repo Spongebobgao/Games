6142cff6bf418e8795091fb45997ea89d26b91a5
Spongebobgao <spongebobgao@gmail.com>
2020-05-03 21:43:23 -0500

    use arrow key to control directions

--- src/components/Tetris.vue
+++ src/components/Tetris.vue
@@ -3,7 +3,7 @@
     <v-container>
       <v-row dense>
         <v-col cols="12">
-          <v-card color="orange" dark>
+          <v-card color="#ffd700" dark>
             <h3>
               Tetris:
               <span
@@ -27,7 +27,7 @@
       </v-row>
     </v-container>
     <div class="tetris-board">
-      <div v-for="m in 16" :key="m+10" class="tetris-grid">
+      <div v-for="m in 17" :key="m+10" class="tetris-grid">
         <div v-for="n in 17" :key="n+10" :id="`${(n+10)}${(m+10)}`"></div>
       </div>
     </div>
@@ -36,7 +36,18 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      allTheshapes: {
+        square: [1119, 1120, 1219, 1220],
+        stick: [1117, 1118, 1119, 1120],
+        lshape: [1118, 1218, 1219, 1220],
+        zshape: [1118, 1119, 1217, 1218],
+        dotshape: [1119, 1218, 1219, 1220]
+      },
+      currentShape: null,
+      currentIndex: null,
+      movedownInterval: null
+    };
   },
   methods: {
     startOver() {
@@ -46,8 +57,65 @@ export default {
     closeModal() {
       document.getElementById("myModal").style.display = "none";
     },
-    startGame() {},
-    resetData() {}
+    startGame() {
+      window.addEventListener("keydown", this.moveAndTranform);
+      this.initShape();
+    },
+    initShape() {
+      const keys = Object.keys(this.allTheshapes);
+      this.currentIndex = keys[Math.floor(Math.random() * keys.length)];
+      this.currentShape = this.allTheshapes[this.currentIndex];
+      this.currentShape.forEach(element => {
+        document.getElementById(element).classList.add(this.currentIndex);
+      });
+      this.movedownInterval = setInterval(this.moveShape, 1000);
+    },
+    moveShape() {
+      this.currentShape.forEach(element => {
+        document.getElementById(element).classList.remove(this.currentIndex);
+      });
+      this.currentShape = this.currentShape.map(
+        element => (element = parseInt(element) + 100)
+      );
+      this.currentShape.forEach(element => {
+        document.getElementById(element).classList.add(this.currentIndex);
+      });
+    },
+    moveAndTranform(e) {
+      if (e.code === "ArrowLeft") {
+        this.currentShape.forEach(element => {
+          document.getElementById(element).classList.remove(this.currentIndex);
+        });
+        this.currentShape = this.currentShape.map(
+          element => (element = parseInt(element) - 1)
+        );
+        this.currentShape.forEach(element => {
+          document.getElementById(element).classList.add(this.currentIndex);
+        });
+      }
+      if (e.code === "ArrowRight") {
+        this.currentShape.forEach(element => {
+          document.getElementById(element).classList.remove(this.currentIndex);
+        });
+        this.currentShape = this.currentShape.map(
+          element => (element = parseInt(element) + 1)
+        );
+        this.currentShape.forEach(element => {
+          document.getElementById(element).classList.add(this.currentIndex);
+        });
+      }
+      if (e.code === "ArrowDown") {
+        this.currentShape.forEach(element => {
+          document.getElementById(element).classList.remove(this.currentIndex);
+        });
+        this.currentShape = this.currentShape.map(
+          element => (element = parseInt(element) + 100)
+        );
+        this.currentShape.forEach(element => {
+          document.getElementById(element).classList.add(this.currentIndex);
+        });
+      }
+    }
   }
 };
 </script>
@@ -60,19 +128,16 @@ h3 {
   display: flex;
   flex-wrap: wrap;
   margin: auto;
-  width: 50%;
+  width: 54%;
   height: 80%;
   margin-bottom: 10px;
-  background-color: #021d1d;
-}
-.tetris-grid {
-  background-color: #f1ddc9;
-  background-blend-mode: multiply;
+  background-color: #fff099;
+  /* border: rgb(174, 218, 226) 1px solid; */
 }
 .tetris-grid div {
   width: 25px;
   height: 25px;
-  border: rgb(224, 211, 150) 1px solid;
+  /* border: rgb(174, 218, 226) 1px solid; */
 }
 .tetris-btn {
   float: right;
@@ -82,5 +147,25 @@ h3 {
   padding: 2px;
   border-radius: 10%;
   margin-right: 40px;
+}
+.square {
+  background-color: teal;
+  border: rgb(174, 218, 226) 1px solid;
+}
+.stick {
+  background-color: rgb(128, 36, 0);
+  border: rgb(174, 218, 226) 1px solid;
+}
+.lshape {
+  background-color: rgb(117, 0, 128);
+  border: rgb(174, 218, 226) 1px solid;
+}
+.zshape {
+  background-color: rgb(0, 23, 128);
+  border: rgb(174, 218, 226) 1px solid;
+}
+.dotshape {
+  background-color: rgb(0, 128, 28);
+  border: rgb(174, 218, 226) 1px solid;
 }
 </style>
